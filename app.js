acea66286e49d98715485761e9d290b75d4a0f85
unknown <mishrakripanshu303@gmail.com>
2022-02-13 15:04:37 +0530

    second commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>{
-    res.status(200).send('welcome to zomato api')
+    res.status(200).send('welcome to lenskart api')
 })
 app.get('/brands',(req,res)=>{
     db.collection('brand name').find().toArray((err,result)=>{
